dc1f44aa83b545e9946c4187225278b43b78fc37
Bibhudutta Das <bibhudutta.das44@gmail.com>
2023-12-06 13:00:13 +0530

    fixed

--- src/pages/termsandservices/Terms.jsx
+++ src/pages/termsandservices/Terms.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 export default function Terms() {
   return (
-    <div className="container mx-auto px-4">
+    <div className="container mx-auto px-4 py-4">
       <h2>I. Acceptance of terms</h2>
       <p>
         Thank you for using Flying Chital. These Terms of Service (the "Terms")
@@ -74,6 +74,283 @@ export default function Terms() {
         content that comes from parties other than Flying Chital or its
         Customers, such as Technology Partners and is available on the Services.
       </p>
+      <h2>Profiles and Clients (s)</h2>
+      <p>
+        "Profiles and Clients" means the Profiles & Clients listed on Flying
+        Chital Platform.
+      </p>
+      <h2>III. Eligibility to use the services</h2>
+      <ul>
+        <li>
+          1. You hereby represent and warrant that you are at least eighteen
+          (18) years of age or above and are fully able and competent to
+          understand and agree the terms, conditions, obligations, affirmations,
+          representations, and warranties set forth in these Terms.
+        </li>
+        <li>
+          2. Compliance with Laws. You are in compliance with all laws and
+          regulations in the country in which you live when you access and use
+          the Services. You agree to use the Services only in compliance with
+          these Terms and applicable law, and in a manner that does not violate
+          our legal rights or those of any third party(ies).
+        </li>
+      </ul>
+      <h2>IV. Changes to the terms</h2>
+      <p>
+        Flying Chital may vary or amend or change or update these Terms, from
+        time to time entirely at its own discretion. You shall be responsible
+        for checking these Terms from time to time and ensure continued
+        compliance with these Terms. Your use of Flying Chital Platform after
+        any such amendment or change in the Terms shall be deemed as your
+        express acceptance to such amended/changed terms and you also agree to
+        be bound by such changed/amended Terms.
+      </p>
+      <h2>V. Translation of the terms</h2>
+      <p>
+        Flying Chital may provide a translation of the English version of the
+        Terms into other languages. You understand and agree that any
+        translation of the Terms into other languages is only for your
+        convenience and that the English version shall govern the terms of your
+        relationship with Flying Chital. Furthermore, if there are any
+        inconsistencies between the English version of the Terms and its
+        translated version, the English version of the Terms shall prevail over
+        others.
+      </p>
+      <h2>VI. Provision of the services being offered by Flying Chital</h2>
+      <ul>
+        <li>
+          1. Flying Chital is constantly evolving in order to provide the best
+          possible experience and information to its Customers. You acknowledge
+          and agree that the form and nature of the Services which Flying Chital
+          provides, may require affecting certain changes in it, therefore,
+          Flying Chital reserves the right to suspend/cancel, or discontinue any
+          or all products or services at any time without notice, make
+          modifications and alterations in any or all of its contents, products
+          and services contained on the site without any prior notice.
+        </li>
+        <li>
+          2. We, the software, or the software application store that makes the
+          software available for download may include functionality to
+          automatically check for updates or upgrades to the software. Unless
+          your device, its settings, or computer software does not permit
+          transmission or use of upgrades or updates, you agree that we, or the
+          applicable software or software application store, may provide notice
+          to you of the availability of such upgrades or updates and
+          automatically push such upgrade or update to your device or computer
+          from time-to-time. You may be required to install certain upgrades or
+          updates to the software in order to continue to access or use the
+          Services, or portions thereof (including upgrades or updates designed
+          to correct issues with the Services). Any updates or upgrades provided
+          to you by us under the Terms shall be considered part of the Services.
+        </li>
+        <li>
+          3. You acknowledge and agree that if Flying Chital disables access to
+          your account, you may be prevented from accessing the Services, your
+          account details or any files or other content, which is contained in
+          your account.
+        </li>
+        <li>
+          4. You acknowledge and agree that while Flying Chital may not
+          currently have set a fixed upper limit on the number of transmissions
+          you may send or receive through the Services, Flying Chital may set
+          such fixed upper limits at any time, at Flying Chital's discretion.
+        </li>
+        <li>
+          5. In our effort to continuously improve the Flying Chital Platform
+          and Services, we undertake research and conduct experiments from time
+          to time on various aspects of the Services and offerings, including
+          our apps, websites, user interface and promotional campaigns. As a
+          result of which, some Customers may experience features differently
+          than others at any given time. This is for making the Flying Chital
+          Platform better, more convenient and easier to use, improving Customer
+          experience, enhancing the safety and security of our services and
+          offerings and developing new services and features.
+        </li>
+        <li>
+          6. By using Flying Chital's Services you agree to the following
+          disclaimers:
+          <ul>
+            <li>
+              o The Content on these Services is for informational purposes
+              only. Flying Chital disclaims any liability for any information
+              that may have become outdated since the last time the particular
+              piece of information was updated. Flying Chital reserves, the
+              right to make changes and corrections to any part of the Content
+              on these Services at any time without prior notice. Unless stated
+              otherwise, all pictures and information contained on these
+              Services are believed to be owned by or licensed to Flying Chital.
+              Please email a takedown request (by using the "Contact Us" link on
+              the home page) to the webmaster if you are the copyright owner of
+              any Content on these Services and you think the use of the above
+              material violates Your copyright in any way. Please indicate the
+              exact URL of the webpage in your request. All images shown here
+              have been digitized by Flying Chital. No other party is authorized
+              to reproduce or republish these digital versions in any format
+              whatsoever without the prior written permission of Flying Chital.
+            </li>
+            <li>
+              o Any certification, licenses or permits ("Certification") or
+              information in regard to such Certification that may be displayed
+              on the Flying Chital Platform is for informational purposes only.
+              Such Certification is displayed by Flying Chital on an 'as
+              available' basis that is provided to Flying Chital by the profiles
+              and clients. Flying Chital does not make any warranties about the
+              validity, authenticity, reliability and accuracy of such
+              Certification or any information displayed in this regard. Any
+              reliance by a Customer upon the Certification or information
+              thereto shall be strictly at such Customer's own risk and Flying
+              Chital in no manner shall assume any liability whatsoever for any
+              losses or damages in connection with the use of this information
+              or for any inaccuracy, invalidity or discrepancy in the
+              Certification or non-compliance of any applicable local laws or
+              regulations by the profiles or clients.
+            </li>
+          </ul>
+        </li>
+        <li>
+          7. Flying Chital reserves, the right to charge a subscription and/or
+          membership fee in respect of any of its product or service and/or any
+          other charge or fee on a per order level from Customers, in respect of
+          any of its product or service on the Flying Chital Platform anytime in
+          future.
+        </li>
+        <li>
+          8. Flying Chital may from time to time introduce referral and/or
+          incentive based programs for its Customers (Program). These Program(s)
+          may be governed by their respective terms and conditions. By
+          participating in the Program, Customers are bound by the Program terms
+          and conditions as well as the Flying Chital Platform terms. Further,
+          Flying Chital reserves the right to terminate / suspend the Customer's
+          account and/or credits / points earned and/or participation of the
+          Customer in the Program if Flying Chital determines in its sole
+          discretion that the Customer has violated the rules of the Program
+          and/or has been involved in activities that are in contravention of
+          the Program terms and/or Flying Chital Platform terms or has engaged
+          in activities which are fraudulent / unlawful in nature. Furthermore,
+          Flying Chital reserves the right to modify, cancel and discontinue its
+          Program without notice to the Customer.
+        </li>
+      </ul>
+      <h2>VII. Use of services by you or Customer</h2>
+      <h2>
+        1. Flying Chital Customer Account Including 'Claim Your Operator
+        profile' Access{" "}
+      </h2>
+      <ui>
+        <li>
+          a. You must create an account in order to use some of the features
+          offered by the Services, including without limitation to 'claim your
+          operator profile' on the Services. Use of any personal information you
+          provide to us during the account creation process is governed by our
+          Privacy Policy. You must keep your password confidential and you are
+          solely responsible for maintaining the confidentiality and security of
+          your account, all changes and updates submitted through your account,
+          and all activities that occur in connection with your account.
+        </li>
+        <li>
+          b. In creating an account and/or claiming your operator profile'
+          listing, you represent to us that all information provided to us in
+          such process is true, accurate and correct, and that you will update
+          your information as and when necessary, in order to keep it accurate.
+          If you are creating an account or claiming a operator profile, then
+          you represent to us that you are the owner or authorized agent of such
+          profile. You may not impersonate someone else, create or use an
+          account for anyone other than yourself, provide an email address other
+          than your own, create multiple accounts or operator profiles except as
+          otherwise authorized by us, or provide or use false information to
+          obtain access to a operator profile' listing on the Services that you
+          are not legally entitled to claim. You acknowledge that any false
+          claiming of a operator profile may cause Flying Chital or third
+          parties to incur substantial economic damages and losses for which you
+          may be held liable and accountable.
+        </li>
+        <li>
+          c. You are also responsible for all activities that occur in your
+          account. You agree to notify us immediately of any unauthorized use of
+          your account in order to enable us to take necessary corrective
+          action. You also agree that you will not allow any third party to use
+          your Flying Chital account for any purpose and that you will be liable
+          for such unauthorized access.
+        </li>
+        <li>
+          d. By creating an account, you agree to receive certain communications
+          in connection with Flying Chital Platform or Services. For example,
+          you might receive comments from other Customers or other Customers may
+          follow the activity to do on your account. You can opt-out or manage
+          your preferences regarding non-essential communications through
+          account settings.
+        </li>
+      </ui>
+      <h2>2. Others Terms</h2>
+      <ul>
+        <li>
+          a. In order to connect you to certain operator profiles, we provide
+          value added telephony services through our phone lines, which are
+          displayed on the specific operator profile listing page on the Flying
+          Chital Platform, which connect directly to operator profiles' phone
+          lines. We record all information regarding this call including the
+          voice recording of the conversation between you, and the operator
+          profile (for internal billing tracking purposes and customer service
+          improvement at the operator profile's end). If you do not wish that
+          your information be recorded in such a manner, please do not use the
+          telephone services provided by Flying Chital. You explicitly agree and
+          permit Flying Chital to record all this information when you avail the
+          telephony services through the Flying Chital provided phone lines on
+          the Flying Chital Platform.
+        </li>
+        <li>
+          b. You agree to use the Services only for purposes that are permitted
+          by (a) the Terms and (b) any applicable law, regulation or generally
+          accepted practices or guidelines in the relevant jurisdictions.
+        </li>
+        <li>
+          c. You agree to use the data owned by Flying Chital (as available on
+          the Services or through any other means like API etc.) only for
+          personal use/purposes and not for any commercial use (other than in
+          accordance with 'Claim Your Operator profile' access) unless agreed to
+          by/with Flying Chital in writing.
+        </li>
+        <li>
+          d. You agree not to access (or attempt to access) any of the Services
+          by any means other than the interface that is provided by Flying
+          Chital, unless you have been specifically allowed to do so, by way of
+          a separate agreement with Flying Chital. You specifically agree not to
+          access (or attempt to access) any of the Services through any
+          automated means (including use of scripts or web crawlers) and shall
+          ensure that you comply with the instructions set out in any robots.txt
+          file present on the Services.
+        </li>
+        <li>
+          e. You agree that you will not engage in any activity that interferes
+          with or disrupts the Services (or the servers and networks which are
+          connected to the Services). You shall not delete or revise any
+          material or information posted by any other Customer(s), shall not
+          engage in spamming, including but not limited to any form of emailing,
+          posting or messaging that is unsolicited.
+        </li>
+      </ul>
+      <h2>VIII. Content</h2>
+      <h2>1. Ownership of Flying Chital Content and Proprietary Rights</h2>
+      <ul>
+        <li>
+          a. We are the sole and exclusive copyright owners of the Services and
+          our Content. We also exclusively own the copyrights, trademarks,
+          service marks, logos, trade names, trade dress and other intellectual
+          and proprietary rights throughout the world (the "IP Rights")
+          associated with the Services and Flying Chital Content, which may be
+          protected by copyright, patent, trademark and other applicable
+          intellectual property and proprietary rights and laws. You acknowledge
+          that the Services contain original works and have been developed,
+          compiled, prepared, revised, selected, and arranged by us and others
+          through the application of methods and standards of judgment developed
+          and applied through the expenditure of substantial time, effort, and
+          money and constitutes valuable intellectual property of us and such
+          others. You further acknowledge that the Services may contain
+          information which is designated as confidential by Flying Chital and
+          that you shall not disclose such information without Flying Chital's
+          prior written consent.
+        </li>
+      </ul>
     </div>
   );
 }
